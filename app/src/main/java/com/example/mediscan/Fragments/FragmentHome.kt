00fcf43370080c0fd0ca5b44b7156e246040cf86
khaylah <khaylahrscholar@gmail.com>
2023-03-11 22:09:05 -0600

    Updates

---
+++ app/src/main/java/com/example/mediscan/Fragments/FragmentHome.kt
@@ -0,0 +1,29 @@
+package com.example.mediscan.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.mediscan.R
+
+//import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_home, container,false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        /*
+        iv_home.setOnClickListener {
+            Toast.makeText(context, "You clicked on the home image", Toast.LENGTH_SHORT).show()
+        }*/
+    }
+
+}
