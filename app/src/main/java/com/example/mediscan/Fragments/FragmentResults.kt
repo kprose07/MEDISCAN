a5bdc69ffb300372d046164a9180637f331dd669
khaylah <khaylahrscholar@gmail.com>
2023-03-15 23:53:15 -0500

    working on new homePage design: 90% through ~Rose

---
+++ app/src/main/java/com/example/mediscan/Fragments/FragmentResults.kt
@@ -0,0 +1,26 @@
+package com.example.mediscan.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.mediscan.R
+
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class ResultsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_results, container,false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
