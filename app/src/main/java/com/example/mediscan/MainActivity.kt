24858bc73b4332f09b885b3e1bf8dd8bb974c39c
khaylah <khaylahrscholar@gmail.com>
2023-03-17 00:29:54 -0500

    Completed the backend dropdown with recycler view. ~Rose

--- app/src/main/java/com/example/mediscan/MainActivity.kt
+++ app/src/main/java/com/example/mediscan/MainActivity.kt
@@ -2,18 +2,14 @@ package com.example.mediscan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
-import com.example.mediscan.Fragments.HomeFragment
-import com.example.mediscan.Fragments.PillsFragment
-import com.example.mediscan.Fragments.ProfileFragment
-import com.example.mediscan.Fragments.SavedFragment
+import com.example.mediscan.Fragments.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val TAG = "MainActivity"
+    val tag = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         val savedFragment = SavedFragment()
         val profileFragment = ProfileFragment()
 
+
         //Sets Home Screen
         setCurrentFragment(homeFragment)
 
@@ -32,29 +29,33 @@ class MainActivity : AppCompatActivity() {
             when(it.itemId){
                 R.id.home ->{
                     setCurrentFragment(homeFragment)
-                    Log.i(TAG,"Home Selected")
+                    Log.i(tag,"Home Selected")
                 }
                 R.id.pills ->{
                     setCurrentFragment(pillsdFragment)
-                    Log.i(TAG,"Pills Selected")
+                    Log.i(tag,"Pills Selected")
                 }
                 R.id.saved ->{
                     setCurrentFragment(savedFragment)
-                    Log.i(TAG,"Saved Selected")
+                    Log.i(tag,"Saved Selected")
                 }
                 R.id.profile ->{
                     setCurrentFragment(profileFragment)
-                    Log.i(TAG,"Profile Selected")
+                    Log.i(tag,"Profile Selected")
                 }
             }
             true
         }
 
+
     }
+
+
     private fun setCurrentFragment(fragment: Fragment) =
         supportFragmentManager.beginTransaction().apply{
             replace(R.id.fl_wrapper,fragment)
             commit()
         }
+
 
 }
--- app/src/main/java/com/example/mediscan/SplashScreen.kt
+++ app/src/main/java/com/example/mediscan/SplashScreen.kt
@@ -26,9 +26,9 @@ class SplashScreen : AppCompatActivity() {
 
         // Handler().postDelayed({
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, Results::class.java)
             startActivity(intent)
             finish()
-        }, 10000) // 3000 is the delayed time in milliseconds.
+        }, 5000) // 3000 is the delayed time in milliseconds.
     }
 }
