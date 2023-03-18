2d48d316d61dd0083f75315ac3b13a721ce8ee9d
Mahlangu <nzundamahlangu@gmail.com>
2023-03-17 23:02:54 -0500

    add medicine data model adn medicine reusable card

--- app/src/main/java/com/example/mediscan/Data/Medicine.kt
+++ app/src/main/java/com/example/mediscan/Data/Medicine.kt
@@ -1,4 +1,8 @@
 package com.example.mediscan.Data
 
-data class Medicine(val name: String, val img_url: String, val common_precs: String) {
-}
+data class Medicine(
+    val name: String,
+    val img_url: String,
+    val common_precs: String
+    )
+
