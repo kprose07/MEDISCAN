d7c7b43dbc70691f258ce7ececa5cf7e118e6700
khaylah <khaylahrscholar@gmail.com>
2023-03-24 00:55:15 -0500

    fixed major warnings

--- app/src/main/java/com/example/mediscan/Fragments/FragmentPills.kt
+++ app/src/main/java/com/example/mediscan/Fragments/FragmentPills.kt
@@ -6,11 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.Fragment
 import com.google.firebase.database.DatabaseReference
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.mediscan.Adapter.NarrowAdapter
-import com.example.mediscan.Data.Medicine
 import com.example.mediscan.Data.NarrowDownSearch
 import com.example.mediscan.R
 import com.google.firebase.database.*
