766fac88e2c2ab1fd70dfda7557df5a2bc91b27a
khaylah <khaylahrscholar@gmail.com>
2023-04-10 19:18:29 -0500

    Firebase stores user data now

---
+++ app/src/main/java/com/example/mediscan/Data/User.kt
@@ -0,0 +1,4 @@
+package com.example.mediscan.Data
+
+class User(val userName: String? = null, val firstName: String? = null, val lastName: String? = null,val email: String? = null, val password: String? = null, val dob: String? = null, val phone: String? = null ) {
+}
--- app/src/main/java/com/example/mediscan/RegisterScreen.kt
+++ app/src/main/java/com/example/mediscan/RegisterScreen.kt
@@ -5,8 +5,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mediscan.Data.User
 import com.example.mediscan.databinding.ActivityRegisterScreenBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_register_screen.*
 import kotlinx.android.synthetic.main.activity_register_screen.login_prompt
 
@@ -15,6 +18,7 @@ class RegisterScreen : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterScreenBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var database : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +32,7 @@ class RegisterScreen : AppCompatActivity() {
         }
 
         binding.registerButton.setOnClickListener{
+            val userName = binding.userUsernameRegInput.text.toString()
             val firstName = binding.regFirstNameInput.text.toString()
             val lastName = binding.regLastNameInput.text.toString()
             val email = binding.regEmailInput.text.toString()
@@ -53,6 +58,30 @@ class RegisterScreen : AppCompatActivity() {
                     "Please fill in ALL fields",
                     Toast.LENGTH_SHORT).show()
             }
+
+            database = FirebaseDatabase.getInstance().getReference("users")
+            val User = User(userName,firstName,lastName, email, password,dob,phone)
+            database.child(userName).setValue(User).addOnSuccessListener {
+                binding.userUsernameRegInput.text?.clear()
+                binding.regFirstNameInput.text?.clear()
+                binding.regLastNameInput.text?.clear()
+                binding.regEmailInput.text?.clear()
+                binding.regPwInput.text?.clear()
+                binding.regDobInput.text?.clear()
+                binding.regPhoneInput.text?.clear()
+                Toast.makeText(
+                    this,
+                    "Saved",
+                    Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(
+                    this,
+                    "Failed",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+
+
         }
     }
 
